09da0c653c2f6090b260ebd3a48b99629cbbf086
ankushsde <ankushs5252@gmail.com>
2024-03-14 12:20:45 +0530

    final push

--- src/components/WeatherDetails/WeatherDetails.jsx
+++ src/components/WeatherDetails/WeatherDetails.jsx
@@ -1,60 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from '@mui/material/Box';
 
-import './WeatherDetails.css'
- 
+
+import "./WeatherDetails.css";
 
 function WeatherDetails() {
-
   const { name } = useParams();
-  const [weatherData, setWeatherData] = useState("")
+  const [weatherData, setWeatherData] = useState("");
+  const [loading, setLoading] = useState(true);
 
-
-  console.log("Data",weatherData.data)
+  console.log("Data", weatherData.data);
 
   const fetchData = async () => {
     try {
-        const response = await fetch(`https://api.weatherbit.io/v2.0/history/energy?city=${name}&start_date=2024-03-03&end_date=2024-03-10&threshold=63&units=I&key=8ff6b1c427824112b02b9f92f1485bbb&tp=daily`)
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        const data =  await response.json()
-        setWeatherData(data)
-    
+      const response = await fetch(
+        `https://api.weatherbit.io/v2.0/history/energy?city=${name}&start_date=2024-03-03&end_date=2024-03-10&threshold=63&units=I&key=8ff6b1c427824112b02b9f92f1485bbb&tp=daily`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+      const data = await response.json();
+      setWeatherData(data);
     } catch (error) {
-      console.log("error in fetching data",error)
+      console.log("error in fetching data", error);
+    } finally {
+      setLoading(false); // Set loading to false when request completes
     }
-  }
+  };
 
   useEffect(() => {
-   fetchData()
+    fetchData();
   }, []);
 
-  function findDay(dateString){
+  function findDay(dateString) {
     const dateObject = new Date(dateString);
     const dayOfWeek = dateObject.getDay();
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
     const dayName = daysOfWeek[dayOfWeek];
-    return dayName
-}
+    return dayName;
+  }
 
   return (
-    
     <div className="weather-details-container">
-    <h1 className="weather-details-title">Weather Details for {name}</h1>
-    {weatherData &&
-      Array.isArray(weatherData.data) &&
-      weatherData.data.map((item, index) => (
-        <div key={index} className="weather-item">
-          <h2 className="weather-temp">
-            Temp: {Math.trunc((item.temp - 32) * 5 / 9)} degree
-          </h2>
-          <h3 className="weather-day">Day: {findDay(item.date)}</h3>
-          <p>Date: {item.date}</p>
+      <h1 className="weather-details-title">Weather Details for {name}</h1>
+      {loading ? (
+        <div>
+         <Box >
+      <CircularProgress />
+    </Box>
         </div>
-      ))}
-  </div>
-  )
+      ) : (
+        <div>
+          {" "}
+          {weatherData &&
+            Array.isArray(weatherData.data) &&
+            weatherData.data.map((item, index) => (
+              <div key={index} className="weather-item">
+                <h2 className="weather-temp">
+                  Temp: {Math.trunc(((item.temp - 32) * 5) / 9)} degree
+                </h2>
+                <h3 className="weather-day">Day: {findDay(item.date)}</h3>
+                <p>Date: {item.date}</p>
+              </div>
+            ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default WeatherDetails
+export default WeatherDetails;
